b167127bdbd6215f0a2b91f67c4d794c84aa457a
Pablo <pablomonardo@gmail.com>
2024-10-21 16:36:15 -0300

    Agrego la función de eliminar las tareas completadas

--- ToDo/index.js
+++ ToDo/index.js
@@ -1,27 +1,34 @@
 const cardSection = document.getElementById("cardSection");
-const doneButton = document.getElementById("done");
+const finishedSection = document.getElementById("finished");
 const submitButton = document.getElementById("submitButton");
 const taskName = document.getElementById('task');
 const taskDescription = document.getElementById('description');
 const taskDate = document.querySelector('input[type="datetime-local"]');
 
+let activeTask = [];
+let finishedTasks = [];
 
-function createNewTask(task) {
-    const {name, description, date} = task;
-    const taskTitle = `<summary>${name} | <span>${date}</span></summary>`;
-    const taskDescription = `<p class="task-description">${description}</p>`
-    const newTask = `<div class="task-card">
-    <details name="faq" close>
-    ${taskTitle} 
-    ${taskDescription}  
-    </details>
-    <input type="button" value="Done" id="done"/>
-    </div>`
 
-    return newTask;
+function createNewTask(task, finished = false) {
+    const {taskID, name, description, date} = task;
+    
+    if(!finished) {
+        return `<div class="task-card" id="${taskID}">
+        <details name="faq" close>
+        <summary>${name} | <span>${date}</span></summary>
+        <p class="task-description">${description}</p> 
+        </details>
+        <input type="button" value="Done" id="done" onclick="taskDone(${taskID})"/>
+        </div>`
+    } else {
+        return `<div class="task-card-finished" id="${taskID}">
+        <details name="faq" close>
+        <summary class="finishedTask">${name} | <span>${date}</span></summary>
+        <p class="task-description">${description}</p> 
+        </details>
+        </div>`
+    }
 }
-
-
 
 submitButton.addEventListener("click", () => {
     const name = taskName.value;
@@ -34,12 +41,14 @@ submitButton.addEventListener("click", () => {
         return
     } 
 
+    const taskID = activeTask.length;
+
     if(date.length < 1) {
         date = new Date().toLocaleDateString()
     }
 
     const newTask = {
-        name, description, date
+        taskID, name, description, date
     };
     
     cardSection.innerHTML += createNewTask(newTask);
@@ -47,7 +56,16 @@ submitButton.addEventListener("click", () => {
     taskName.value = "";
     taskDescription.value = "";
     taskDate.value = "";
-    
+    activeTask.push(newTask);
 })
 
+function taskDone(id) {
+    const currentCard = document.getElementById(`${id}`);
+    let finishedTask = activeTask.find((element) => element.taskID === id);
+    finishedTask.tasID = finishedTasks.length;
+    finishedTasks.push(finishedTask);
 
+    finishedSection.innerHTML += createNewTask(finishedTask, true);
+
+    currentCard.remove()
+}
